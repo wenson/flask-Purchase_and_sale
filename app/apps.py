6fb845da59e4b25737b67d344cbcb4185e93958c
agamgn <agamgn@163.com>
2019-04-25 14:16:04 +0800

    Missing file

--- app/apps.py
+++ app/apps.py
@@ -11,7 +11,7 @@ app=Flask(__name__)
 
 
 # 数据库配置
-app.config["SQLALCHEMY_DATABASE_URI"]="mysql+pymysql://uesr:password@127.0.0.1:3306/dbname?charset=utf8"
+app.config["SQLALCHEMY_DATABASE_URI"]="mysql+pymysql://user:password@127.0.0.1:3306/dbname?charset=utf8"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=True
 # 密钥配置，在生产环境中使用系统自动生成
 app.config['SECRET_KEY']='d890fbe7e26c4c3eb557b6009e3f4d3d'
